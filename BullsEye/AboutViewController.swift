d10ee0f03f0414c93597dabaf1fad62fd001b354
Xueting Tan <xuetingt@student.unimelb.edu.au>
2018-12-05 03:15:08 +1100

    add files

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Sue Tan on 4/12/18.
+//  Copyright © 2018 Sue Tan. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ BullsEye/AuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AuthorViewController.swift
+//  BullsEye
+//
+//  Created by Sue Tan on 5/12/18.
+//  Copyright © 2018 Sue Tan. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
